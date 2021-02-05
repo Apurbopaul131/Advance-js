a429905de4dc15c6e790580bcb6cf68a08f3b582
Apurbopaul131 <apurbopaul131@gmail.com>
2021-02-05 21:11:36 +0600

    add for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+// let num;
+// console.log(num);
+// function add(num1,num2){
+//     console.log(num1 + num2);
+// }
+// const result = add(10,20);
+// console.log(result);
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+// add(10);
+// const person = {
+//     name:"Apurbo paul",
+//     phone:"01629217129",
+// }
+// let {name,phone,gf} = person;
+// console.log(phone);
+// console.log(gf);
+// let num = undefined;
+// console.log(num);
+// let array = [10,12,15];
+// console.log(array[10]);
+let num = null;
+console.log(num);
